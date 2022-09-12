2741c19af11ff767f01ce0ddbf343fb426733c20
Michael Puckett <30637257+JustASpeedrunner@users.noreply.github.com>
2022-09-12 02:40:27 +0000

    vscode won't sync so uploading updated files

--- slides.md
+++ slides.md
@@ -22,4 +22,33 @@
 - Due to Henry's boyish appearance and relatively young age compared to his outlaw peers he got the nickname, "The Kid."
 - In 1877, Henry got into an argument with a blacksmith at a saloon who had a history of bullying Henry. During the argument the blacksmith called Henry a pimp, and in retaliation Henry called him a son of a bitch.
 - After this Henry was thrown to the ground and was getting the tar beat out of him. Henry tried to reach for the blacksmith's gun and the two began fighting over the gun, and eventually Henry got the gun in his hands and he shot and killed the blacksmith.
-- He fled the town
+- He fled the town though he returned a few days later, he was then arrested and held in Camp Grant. He again managed to slip through the bars and move to New Mexico after stealing a horse.
+- He was ambushed by Apache and had to walk the rest of the way to Fort Stanton, he turned up there nearly dead from dehydration and he was nursed back to health by John Jone's mother which was a friend he knew there.
+- Henry later travelled to Apache Tejo, an abandoned army post that was then occupied by a gang of cattle rustlers.
+- This is when Henry began using the alias, William H. Bonney. William was then shortened to Billy, giving Henry the now infamous nickname, Billy the Kid.
+
+# Pre-Lincoln County War
+- Billy found work as a rancher for John Henry Tunstall, attempting to become a cattle magnate and merchant in Lincoln County along with his partner, Alexander McSween.
+- The problem with this being that there was already a cattle and merchant outfit in Lincoln County, ran by two men called Lawrence Murphy and James Dolan. This outfit was named \"The House.\"
+- Tunstall would constantly clash with The House because they were trying to push him out and maintain their monopoly by buying up all the resources and even threatening Tunstall's clients.
+- This tension would continue and an event that affected the two sellers would snowball into what would later be called the Lincoln County War.
+- In 1878, Emil Fritz passed away and he had a life insurance policy along with a will which would decide to whom the money would go to. 
+- McSween, being a lawyer, was hired to manage the accounts and payout to whom was to be payed.
+- Upon review McSween noticed that everything in Fritz's name was to go to Lawrence Murphy. He was not too fond of the idea of giving his business competitor who had repeatedly harrassed both McSween, Tunstall, and his patrons. So, McSween refused to pay Murphy.
+- Murphy obtained a court order that allowed them to seize assets from McSween to gather the money that he was supposed to be payed. This court order, however, did not say they could only seize McSween's assets, but Tunstall's as well. 
+- The House saw this as a golden opportunity to go into their business and take anything and everything they wanted to prevent their business from being able to compete with them.
+- Eventually, to stop Tunstall's cattle from being constantly stolen, he and his ranchers gathered their cattle to move them to a hidden place in which the gangs that The House had hired could not find it.
+- What Tunstall didn't know was that a possee was formed to seize the cattle in the name of The House, lead by William J. Brady--the sheriff.
+- When Tunstall saw the possee on his land he told his ranchers, including Billy the Kid, to stay with the cattle and that he would go see the possee by himself.
+- So they stayed, as Tunstall was approaching them he was shot in the chest and he fell from his horse. As Tunstall laid on the ground one of the members of the possee walked over to him and took his gun, he then shot Tunstall in the head with his own gun. All of this in full view of Billy and the other ranchers.
+- The ranchers swore they would get revenge on The House and sheriff Brady for murdering John Tunstall.
+
+# Lincoln County War
+- Billy the Kid and his associate Dick Brewer swore affidavits against Brady and his possee to justice of the peace, John B. Wilson, saying that they had witnessed him commit a murder and that he had gone rogue and was abusing his powers as sheriff.
+- Wilson then deputised Billy, Dick Brewer, and the rest of the ranchers. Then issued murder warrants for the murderers of Tunstall.
+- Billy and the ranchers created a possee called The Regulators consisting of the now deputised ranchers.
+- In February of 1878 Billy and Brewer saw Brady and his possee, so they rode up to them and told them that they were deputised and that they were there to arrest Brady and his men.
+- Brady and his men said, "No you," and arrested Billy and Brewer. During the arrest Brady confiscated Billy's treasured Winchester rifle.
+- Billy was typically known for being level-headed but he was absolutely furious that Brady had stolen his rifle.
+- Later The Regulators had broken Billy and Brewer out of the jail and put the guards into the cells.
+- 
--- src/slides.rs
+++ src/slides.rs
@@ -27,7 +27,40 @@ pub fn mainslide() {
         stdout(),
         SetForegroundColor(Color::White),
         SetBackgroundColor(Color::Black),
-        Print("▄▀█  █▀█ █▀█ █▀█ ░░█ █▀▀ █▀▀ ▀█▀  █▀▀ █▀█ █▀█  █░█ █ █▀ ▀█▀ █▀█ █▀█ █▄█  ▀█▀ █▀█  █▀█ █▀█ █▀▀ █▀ █▀▀ █▄░█ ▀█▀\r\n█▀█  █▀▀ █▀▄ █▄█ █▄█ ██▄ █▄▄ ░█░  █▀░ █▄█ █▀▄  █▀█ █ ▄█ ░█░ █▄█ █▀▄ ░█░  ░█░ █▄█  █▀▀ █▀▄ ██▄ ▄█ ██▄ █░▀█ ░█░"),
+        Print("
+
+        ▄▀█  █▀█ █▀█ █▀█ ░░█ █▀▀ █▀▀ ▀█▀  █▀▀ █▀█ █▀█  █░█ █ █▀ ▀█▀ █▀█ █▀█ █▄█  ▀█▀ █▀█  █▀█ █▀█ █▀▀ █▀ █▀▀ █▄░█ ▀█▀  ▄▀█ █▄░█ █▀▄  ▀█▀ █▀▀ ▄▀█ █▀▀ █░█  ▄▀█ █▄▄ █▀█ █░█ ▀█▀\r
+        █▀█  █▀▀ █▀▄ █▄█ █▄█ ██▄ █▄▄ ░█░  █▀░ █▄█ █▀▄  █▀█ █ ▄█ ░█░ █▄█ █▀▄ ░█░  ░█░ █▄█  █▀▀ █▀▄ ██▄ ▄█ ██▄ █░▀█ ░█░  █▀█ █░▀█ █▄▀  ░█░ ██▄ █▀█ █▄▄ █▀█  █▀█ █▄█ █▄█ █▄█ ░█░\r
+
+                                                                    ▀█▀ █░█ █▀▀  █▀ ▀█▀ █▀█ █▀█ █▄█  █▀█ █▀▀\r
+                                                                    ░█░ █▀█ ██▄  ▄█ ░█░ █▄█ █▀▄ ░█░  █▄█ █▀░\r\n"),
+        ResetColor
+    ).ok();
+
+    sleep(Duration::from_secs(2));
+    
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::White),
+        SetBackgroundColor(Color::Black),
+        Print("
+                                                ██████╗░██╗██╗░░░░░██╗░░░░░██╗░░░██╗  ████████╗██╗░░██╗███████╗  ██╗░░██╗██╗██████╗░\r
+                                                ██╔══██╗██║██║░░░░░██║░░░░░╚██╗░██╔╝  ╚══██╔══╝██║░░██║██╔════╝  ██║░██╔╝██║██╔══██╗\r
+                                                ██████╦╝██║██║░░░░░██║░░░░░░╚████╔╝░  ░░░██║░░░███████║█████╗░░  █████═╝░██║██║░░██║\r
+                                                ██╔══██╗██║██║░░░░░██║░░░░░░░╚██╔╝░░  ░░░██║░░░██╔══██║██╔══╝░░  ██╔═██╗░██║██║░░██║\r
+                                                ██████╦╝██║███████╗███████╗░░░██║░░░  ░░░██║░░░██║░░██║███████╗  ██║░╚██╗██║██████╔╝\r
+                                                ╚═════╝░╚═╝╚══════╝╚══════╝░░░╚═╝░░░  ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  ╚═╝░░╚═╝╚═╝╚═════╝░\r"),
+        ResetColor
+    ).ok();
+    sleep(Duration::from_secs(2));
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::White),
+        SetBackgroundColor(Color::Black),
+        Print("\n\n
+                                                                - Hit Enter to continue to the next slide.\r\n
+                                                                - For the sources hit the 's' key.\r\n
+                                                                - Use Esc to quit\r"),
         ResetColor).ok();
 }
 
@@ -38,15 +71,23 @@ pub fn second() {
         SetForegroundColor(Color::White),
         SetBackgroundColor(Color::Black),
         Print("
-    - Billy the Kid was born as Henry McCarty in NYC in 1859\r
-    - Born to Catherine and Patrick McCarty soon having a younger brother named Joseph\r
-    - His father suddenly passed away and Catherine took herself and her two sons to Indianapolis\r
-    - During this time Catherine began a romance with a William Antrim\r
-    - Soon after the four moved to Wichita, Kansas, in 1870\r
-    - They then moved to Santa Fe, where Henry's mom would marry Antrim in 1873, and later that same year they moved to Silver City, New Mexico\r
-    - A year later Henry's mom died of tuberculosis leaving Henry and Joseph in the care of their step-father\r
-    - William would beat Henry and with the death of Henry's mother they became even worse as she wasn't there to protect her child.\r
-    - Henry left home and began to live in a boarding house in which he would do work in exchange for living quarters.\r"),
+░░███╗░░██╗░░  ███████╗░█████╗░██████╗░██╗░░░░░██╗░░░██╗  ██╗░░░░░██╗███████╗███████╗\r
+░████║░░╚██╗░  ██╔════╝██╔══██╗██╔══██╗██║░░░░░╚██╗░██╔╝  ██║░░░░░██║██╔════╝██╔════╝\r
+██╔██║░░░╚██╗  █████╗░░███████║██████╔╝██║░░░░░░╚████╔╝░  ██║░░░░░██║█████╗░░█████╗░░\r
+╚═╝██║░░░██╔╝  ██╔══╝░░██╔══██║██╔══██╗██║░░░░░░░╚██╔╝░░  ██║░░░░░██║██╔══╝░░██╔══╝░░\r
+███████╗██╔╝░  ███████╗██║░░██║██║░░██║███████╗░░░██║░░░  ███████╗██║██║░░░░░███████╗\r
+╚══════╝╚═╝░░  ╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░  ╚══════╝╚═╝╚═╝░░░░░╚══════╝\r
+
+        - Billy the Kid was born as Henry McCarty in NYC in 1859\r\n
+        - Born to Catherine and Patrick McCarty soon having a younger brother named Joseph\r\n
+        - His father suddenly passed away and Catherine took herself and her two sons to Indianapolis\r\n
+        - During this time Catherine began a romance with a William Antrim\r\n
+        - Soon after the four moved to Wichita, Kansas, in 1870\r\n
+        - They then moved to Santa Fe, where Henry's mom would marry Antrim in 1873, and later that same year they moved to Silver City, New Mexico\r\n
+        - A year later Henry's mom died of tuberculosis leaving Henry and Joseph in the care of their step-father\r\n
+        - William would beat Henry and with the death of Henry's mother they became even worse as she wasn't there to protect her child.\r\n
+        - Henry left home and began to live in a boarding house in which he would do work in exchange for living quarters.\r\n
+        - He would make a very small amount of money here so he could hardly pay for proper food.\r\n"),
         ResetColor).ok();
 }
 
@@ -56,7 +97,34 @@ pub fn third() {
         stdout(),
         SetForegroundColor(Color::White),
         SetBackgroundColor(Color::Black),
-        Print("deez"),
+        Print("
+██████╗░██╗░░  ███████╗██╗██████╗░░██████╗████████╗  ░█████╗░██████╗░██╗███╗░░░███╗███████╗░██████╗\r
+╚════██╗╚██╗░  ██╔════╝██║██╔══██╗██╔════╝╚══██╔══╝  ██╔══██╗██╔══██╗██║████╗░████║██╔════╝██╔════╝\r
+░░███╔═╝░╚██╗  █████╗░░██║██████╔╝╚█████╗░░░░██║░░░  ██║░░╚═╝██████╔╝██║██╔████╔██║█████╗░░╚█████╗░\r
+██╔══╝░░░██╔╝  ██╔══╝░░██║██╔══██╗░╚═══██╗░░░██║░░░  ██║░░██╗██╔══██╗██║██║╚██╔╝██║██╔══╝░░░╚═══██╗\r
+███████╗██╔╝░  ██║░░░░░██║██║░░██║██████╔╝░░░██║░░░  ╚█████╔╝██║░░██║██║██║░╚═╝░██║███████╗██████╔╝\r
+╚══════╝╚═╝░░  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░  ░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝╚══════╝╚═════╝░\r
+
+        - In 1875, Henry was caught stealing food,\r\n
+        - Ten days later he and an accomplice, named Sombrero Jack, robbed a Chinese laundromat in which they took some clothes and two pistols.\r\n
+        - Henry hid the goods in the boarding house but they were later found and Henry was reported to the police.\r\n
+        - Henry was arrested and while in the jailhouse he was skinny enough to squeeze between the bars of his cell and he shimmied up a chimney to get to the roof and escape.\r\n
+        - Henry fled to his step-father's house and after just a few days he was kicked out. Henry robbed his step-father and took serveral clothing items as well as several guns,\r\n
+            marking the last time he and his step-father ever saw each other.\r\n
+        - After this Henry began using the name Henry Antrim, which was likely to help him evade authorities.\r\n
+        - Henry fled to  South-eastern Arizona, where he would become a ranch hand for Henry Hooker. He would typically blow all of his wages by gambling.\r\n
+        - While working under Hooker, Henry met John Mackie, an ex-military cavalry private turned criminal, and they would steal horses from a nearby U.S. Army outpost, Camp Grant.\r\n
+        - Due to Henry's boyish appearance and relatively young age compared to his outlaw peers he got the nickname, \"The Kid.\"\r\n
+        - In 1877, Henry got into an argument with a blacksmith at a saloon who had a history of bullying Henry. During the argument the blacksmith called Henry a pimp, and in\r\n
+            retaliation Henry called him a son of a bitch.\r\n
+        - After this Henry was thrown to the ground and was getting the tar beat out of him. Henry tried to reach for the blacksmith's gun and the two began fighting over the gun,\r\n
+            and eventually Henry got the gun in his hands and he shot and killed the blacksmith.\r\n
+        - He fled the town though he returned a few days later, he was then arrested and held in Camp Grant. He again managed to slip through the bars and move to New Mexico after\r\n
+            stealing a horse.\r\n
+        - He was ambushed by Apache and had to walk the rest of the way to Fort Stanton, he turned up there nearly dead from dehydration and he was nursed back to health by\r\n
+            John Jone's mother which was a friend he knew there.\r\n
+        - Henry later travelled to Apache Tejo, an abandoned army post that was then occupied by a gang of cattle rustlers.\r\n
+        - This is when Henry began using the alias, William H. Bonney. William was then shortened to Billy, giving Henry the now infamous nickname, Billy the Kid.\r\n"),
         ResetColor).ok();
 }
 pub fn fourth() {
@@ -107,7 +175,17 @@ pub fn sources() {
         stdout(),
         SetForegroundColor(Color::White),
         SetBackgroundColor(Color::Black),
-        Print("# Sources\r\n\"Absolute Mad Lads - Billy the Kid\" YouTube, uploaded by Count Dankula, 13-12-2019, https://www.youtube.com/watch?v=QjWqfV3j-eY\"\r\nGarrett, Pat F. (1882). The Authentic Life of Billy, the Kid (1st ed.). Santa Fe, New Mexico: New Mexican Printing and Publishing Company. OCLC 748293298.\r\n[Billy the Kid photograph taken by Ben Wittick in 1879 or 1880.](https://commons.wikimedia.org/wiki/File:Billy_the_Kid_tintype,_Fort_Sumner,_1879-80-Edit2.jpg)"),
+        Print("
+░██████╗░█████╗░██╗░░░██╗██████╗░░█████╗░███████╗░██████╗\r
+██╔════╝██╔══██╗██║░░░██║██╔══██╗██╔══██╗██╔════╝██╔════╝\r
+╚█████╗░██║░░██║██║░░░██║██████╔╝██║░░╚═╝█████╗░░╚█████╗░\r
+░╚═══██╗██║░░██║██║░░░██║██╔══██╗██║░░██╗██╔══╝░░░╚═══██╗\r
+██████╔╝╚█████╔╝╚██████╔╝██║░░██║╚█████╔╝███████╗██████╔╝\r
+╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚═╝░╚════╝░╚══════╝╚═════╝░\r
+
+        \"Absolute Mad Lads - Billy the Kid\" YouTube, uploaded by Count Dankula, 13-12-2019, https://www.youtube.com/watch?v=QjWqfV3j-eY\"\r\n
+        Garrett, Pat F. (1882). The Authentic Life of Billy, the Kid (1st ed.). Santa Fe, New Mexico: New Mexican Printing and Publishing Company. OCLC 748293298.\r\n
+        [Billy the Kid photograph taken by Ben Wittick in 1879 or 1880.](https://commons.wikimedia.org/wiki/File:Billy_the_Kid_tintype,_Fort_Sumner,_1879-80-Edit2.jpg)"),
         ResetColor).ok();
 }
 
